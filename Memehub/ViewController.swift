a7e2b52b860dcb9426838c76c933b9724f1ca147
Shahrez Jan <snjan19@bu.edu>
2016-10-22 20:07:10 -0400

    DESPARARION

--- Memehub/ViewController.swift
+++ Memehub/ViewController.swift
@@ -9,16 +9,21 @@
 import UIKit
 
 class ViewController: UIViewController {
+	
+	//MARK: Properties
 
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		// Do any additional setup after loading the view, typically from a nib.
+	
 	}
 
 	override func didReceiveMemoryWarning() {
 		super.didReceiveMemoryWarning()
 		// Dispose of any resources that can be recreated.
 	}
+	
+	
 
 
 }
